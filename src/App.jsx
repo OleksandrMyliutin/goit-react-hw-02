313116b6bab6bf3e2619f6a1f188005f568da540
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-03-30 01:08:49 +0200

    add localStorage use

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import Description from './components/Description/Description'
 import Options from './components/Options/Options'
 import Feedback from './components/Feedback/Feedback'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => { 
-  const [clicks, setClicks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
+  const marks = {
+    good:0,
+    neutral:0,
+    bad:0
+  }
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = localStorage.getItem("saved-clicks");
+    return savedClicks ? JSON.parse(savedClicks) : marks;
   });
+  
+  useEffect(() => {
+    localStorage.setItem("saved-clicks", JSON.stringify(clicks));
+  }, [clicks]);
   
   const updateFeedback = feedbackType => {
     setClicks({
@@ -18,15 +26,13 @@ const App = () => {
     console.log(feedbackType);
     
   };
+  
   const totalFeedback = clicks.good + clicks.neutral + clicks.bad;
 
   const positive = Math.round((clicks.good/totalFeedback)*100);
+
   const resetUpdate = () => {
-    setClicks({
-      good: 0,
-      neutral: 0,
-      bad: 0
-    });
+    setClicks(marks);
   };
   return (
     <>
